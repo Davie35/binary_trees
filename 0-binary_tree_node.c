df671e3e88ae891450a18e4437ffd66c0ff0f6d5
Davie35 <79510769+Davie35@users.noreply.github.com>
2022-08-24 13:19:15 +0300

    function that creates a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create a node
+ * @parent: pointer to parent
+ * Return: pointer to the new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *nodo;
+
+nodo = malloc(sizeof(binary_tree_t));
+if (nodo == NULL)
+return (NULL);
+nodo->n = value;
+nodo->parent = parent;
+nodo->left = NULL;
+nodo->right = NULL;
+return (nodo);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
